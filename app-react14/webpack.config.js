5887fd5a400f2a9e3f4a733e95f171da962e6c5f
Grgur Grisogono <grgur@grgur.com>
2016-07-19 12:39:25 +0200

    Webpack config cleanup

--- app-react14/webpack.config.js
+++ app-react14/webpack.config.js
@@ -1,7 +1,6 @@
 /* global __dirname */
 
 var path = require('path');
-
 var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -10,35 +9,39 @@ var dir_html = path.resolve(__dirname, 'html');
 var dir_build = path.resolve(__dirname, 'build');
 
 module.exports = {
-    entry: path.resolve(dir_js, 'main.js'),
-    output: {
-        path: dir_build,
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: dir_build,
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel',
-                test: dir_js,
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: dir_html }
-        ]),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: '"production"'
-          }
-        }),
-    ],
-    stats: {
-        colors: true
-    },
-    devtool: 'eval',
+  entry: path.resolve(dir_js, 'main.js'),
+  output: {
+    path: dir_build,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: dir_build,
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: dir_js,
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+        from: dir_html
+    }]),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    // UglifyJS won't work well with ES6 code
+    // new webpack.optimize.UglifyJsPlugin({
+    //   sourceMap: false,
+    //   warnings: false,
+    // }),
+  ],
+  stats: {
+    colors: true,
+  },
 };
